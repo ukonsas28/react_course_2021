4726f45c114d75a29ba744df055c3372d6b3173b
Alexey Shirokov <shirokovam@lad24.ru>
2021-12-05 14:53:25 +0300

    initial prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = function () {
   return (
@@ -9,17 +9,14 @@ const App = function () {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
           <code>src/App.tsx</code>
-          {' '}
           and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
