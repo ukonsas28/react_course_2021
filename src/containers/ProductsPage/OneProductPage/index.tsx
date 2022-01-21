d141523b4a29c8300a0e56e5975c2d65d4675a47
Alexey Shirokov <shirokovam@lad24.ru>
2022-01-22 01:21:56 +0300

    add routing in app

---
+++ src/containers/ProductsPage/OneProductPage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsPage: React.FC = () => {
+  return <h1>PRODUCT LIST</h1>;
+};
+
+export default ProductsPage;
---
+++ src/containers/ProductsPage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductPage: React.FC = () => {
+  return <h1>PRODUCT LIST</h1>;
+};
+
+export default ProductPage;
