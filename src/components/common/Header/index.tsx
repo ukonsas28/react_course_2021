02e5c685b0ba606848f95fd3c7c57ae2f2cf9a0c
Alexey Shirokov <shirokovam@lad24.ru>
2022-01-22 11:17:18 +0300

    add base routing in app

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./Header.module.scss";
 
 const Header: React.FC = () => {
+  console.log("render");
+
   return (
     <header className={style["wrapper-example"]}>
-      <div className={style.logo}>
-        <img src="/logo.png" alt="logo" />
-      </div>
+      <NavLink to="/">
+        <div className={style.logo}>
+          <img src="/logo.png" alt="logo" />
+        </div>
+      </NavLink>
       <nav>
         <ul className={style.menu_list}>
-          <li>About</li>
-          <li>Content</li>
-          <li>Help</li>
+          <li>
+            <NavLink to="/">Main</NavLink>
+          </li>
+          <li>
+            <NavLink to="/posts">Posts</NavLink>
+          </li>
+          <li>
+            <NavLink to="/registration">Registration</NavLink>
+          </li>
         </ul>
       </nav>
       <button type="button" className={style.btn}>
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import PageWrapper from "../../components/common/PageWrapper";
 import MainPage from "../MainPage";
+import RegistrationPage from "../RegistrationPage";
 
 const App = () => {
-  useEffect(() => {
-    console.log("app mount");
-  }, []);
-  return <MainPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<PageWrapper />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="*" element={<h1>404 not found</h1>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
